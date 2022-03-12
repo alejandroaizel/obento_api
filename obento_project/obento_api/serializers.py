0466bf14bd43fc52a60107ae199754c7b608d203
Miguel E. Ruiz <meruiz77@gmail.com>
2022-03-12 21:04:14 +0100

    F #4: Add register User, login and logout
    We use Django User as user in the API

--- obento_project/obento_api/serializers.py
+++ obento_project/obento_api/serializers.py
@@ -1,6 +1,9 @@
 # serializers.py
 
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
 
 from .models import Recipe, Ingredient, Compound, RecipeCategory
 
@@ -26,7 +29,7 @@ class RecipeSerializer(serializers.ModelSerializer):
                   'cooking_time',
                   'is_lunch',
                   'image_path')
-        
+
     def create(self, validated_data):
         ingredients_data = validated_data.pop('ingredients')
         recipe_category = RecipeCategory.objects.get(pk=validated_data['category'])
@@ -36,7 +39,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         for ingredient_data in ingredients_data:
             ingredient = Ingredient.objects.get(pk=ingredient_data['ingredient_id'])
             Compound.objects.create(recipe=recipe, ingredient=ingredient, quantity=ingredient_data['quantity'])
-            
+
         return recipe.id
 
 
@@ -50,3 +53,30 @@ class IngredientSerializer(serializers.HyperlinkedModelSerializer):
                   'unitary_price',
                   'kcalories',
                   'icon_name')
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+            required=True,
+            validators=[UniqueValidator(queryset=User.objects.all())]
+            )
+
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+
+    class Meta:
+        model = User
+        fields = ('password', 'email', 'first_name')
+        extra_kwargs = {
+            'username': {'required': True},
+            'first_name': {'required': True}
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            email=validated_data['email'],
+            username=validated_data['email'],
+            first_name=validated_data['first_name']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
--- obento_project/obento_api/urls.py
+++ obento_project/obento_api/urls.py
@@ -4,14 +4,22 @@ from django import urls
 from django.urls import include, path
 from rest_framework import routers
 from . import views
+from obento_api.views import RegisterView, LogoutAllView, LogoutView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 router = routers.DefaultRouter()
+
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('ingredients', views.ingredients_list),
     path('recipes', views.recipes_list),
     path('recipes/<int:recipe_id>', views.get_delete_recipe)
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='auth_register'),
+    path('logout/', LogoutView.as_view(), name='auth_logout'),
+    path('logout_all/', LogoutAllView.as_view(), name='auth_logout_all'),
 ]
