38344192f9ead9c82abcc86f9bd3a4eb3336e171
Miguel E. Ruiz <meruiz77@gmail.com>
2022-03-15 17:30:46 +0100

    F #4: Fix URLS of application

--- obento_project/obento_api/urls.py
+++ obento_project/obento_api/urls.py
@@ -15,11 +15,11 @@ urlpatterns = [
     path('', include(router.urls)),
     path('ingredients', views.ingredients_list),
     path('recipes', views.recipes_list),
-    path('recipes/<int:recipe_id>', views.get_delete_recipe)
+    path('recipes/<int:recipe_id>', views.get_delete_recipe),
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='auth_register'),
     path('logout/', LogoutView.as_view(), name='auth_logout'),
-    path('logout_all/', LogoutAllView.as_view(), name='auth_logout_all'),
+    path('logout_all/', LogoutAllView.as_view(), name='auth_logout_all')
 ]
