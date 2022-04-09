425de07a5d3e6d24a108af28336a6f67f801da65
José Luis Martín Pérez <pepe9817@gmail.com>
2022-04-09 17:17:58 +0200

    Changed base settings

--- obento_project/obento_project/settings/base.py
+++ obento_project/obento_project/settings/base.py
@@ -78,12 +78,12 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-            # 'OPTIONS': {
-            #     'read_default_file': '/etc/mysql/my.cnf',
-            # },
-        'USER': 'obento-admin',
-        'PASSWORD': 'Admin1234?',
-        'NAME': 'obento_database'
+            'OPTIONS': {
+                'read_default_file': '/etc/mysql/my.cnf',
+            },
+        # 'USER': 'obento-admin',
+        # 'PASSWORD': 'Admin1234?',
+        # 'NAME': 'obento_database'
     }
 }
 
